6d8d07821835477e6d5a5d3993c010c8c1ca318e
joaomiguel1803 <j.gogola@escola.pr.gov.br>
2026-06-10 16:18:07 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function mostrarMensagem() {
+ alert(
+ "💧 Feche a torneira ao escovar os dentes!\n\n" +
+ "🌱 Regue as plantas sem desperdício!\n\n" +
+ "🚿 Tome banhos rápidos!\n\n" +
+ "😊 Vamos cuidar da água juntos!"
+ );
+}
